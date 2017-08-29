869da660b4173b363a556a2bb8eb8bdf2278c057
Hackbright Student <no-reply@hackbrightacademy.com>
2017-08-28 17:20:02 -0700

    Simplified dictionary

--- ratings.py
+++ ratings.py
@@ -5,20 +5,25 @@ import sys
 
 restaurants = {}
 
+def import_and_split():
 
-filename = sys.argv[1]
-for line in open(filename):
-    line = line.strip()
-    words = line.split(":")
-    restaurants[words[0]] = words[1]
+    restaurants = {}
 
+    filename = sys.argv[1]
+    for line in open(filename):
+        line = line.strip()
+        words = line.split(":")
+        restaurants[words[0]] = words[1]
 
-for name_of_restaurant in sorted(restaurants):
-    print '{} is rated at {}.'.format(name_of_restaurant, restaurants[name_of_restaurant])
-
-
-
+    return restaurants
 
 
+def sorting_restaurants(dict):
+
+    for name_of_restaurant in sorted(restaurants):
+        return '{} is rated at {}.'.format(name_of_restaurant, restaurants[name_of_restaurant])
+
+
+sorting_restaurants(restaurants)
 
 
